ae931cc35a6a0a4ed1e4bccb03ee6ac8d36cb46d
Richard <richardpablop@gmail.com>
2015-06-25 18:13:55 +0100

    First commit

---
+++ src/main/java/com/richard/gradle/gradletest/App.java
@@ -0,0 +1,11 @@
+package com.richard.gradle.gradletest;
+
+/**
+ * Created by rpad0002 on 25/06/15.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
